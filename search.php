5f814b0343bb973918a2c8e573ef43f6f4f2cc80
RickyV02 <VLLRCR02E30C351Q@studium.unict.it>
2024-05-15 19:11:31 +0200

    prova

---
+++ search.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Letterboxd•Search&Results</title>
+    <link rel="stylesheet" href="search.css">
+</head>
+
+<body>
+    <header>
+        <h1>Results for "METTI IL PHP DELLA COSA CERCATA"</h1>
+        <form>
+            <input type="text" autocomplete="off" id="movie_name">
+            <input type="submit" class="submit" value="">
+        </form>
+    </header>
+    <main>
+        <div class="movie-card">
+            <img src="movie1.jpg">
+            <h2>Movie 1 Title</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div class="movie-card">
+            <img src="movie2.jpg">
+            <h2>Movie 2 Title</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <!-- AGGIUNGERE QUA I RISULTATI -->
+    </main>
+</body>
+
+</html>
