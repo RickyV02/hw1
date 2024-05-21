7603b4c967493f8972991fa3574e5c3d59bd6715
RickyV02 <VLLRCR02E30C351Q@studium.unict.it>
2024-05-21 18:39:38 +0200

    home game

--- result.php
+++ result.php
@@ -33,9 +33,32 @@ const search_qid = "<?php echo $searchqid; ?>";
 
 <body>
     <header>
+        <h1>Here's what we found</h1>
     </header>
     <section id="modal_search">
     </section>
+    <footer>
+        <div class="footer-content">
+            <div class="footer-section">
+                <h3>About Us</h3>
+                <p>FlixNexus • Social Discovery</p>
+            </div>
+            <div class="footer-section">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="terms.html">Terms of Use</a></li>
+                </ul>
+            </div>
+            <div class="footer-section">
+                <h3>Contact Us</h3>
+                <p>Email: FlixNexus@gmail.com</p>
+                <p>Phone: 3420710390</p>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
