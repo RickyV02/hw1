886e9e0683c6f112b5071c043617d6ad6ebb04fc
RickyV02 <VLLRCR02E30C351Q@studium.unict.it>
2024-05-29 10:07:40 +0200

    working on emails

--- credentials.php
+++ credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-$apikey = "e0c23c4af0msh3480f7850af0571p15f66bjsn296832cea7b1";
+$apikey = "830db3a87cmshfeaf34c8adc5545p17bfe6jsn44a5876cef91";
 $client_id_twitch = "12h3bsf6as2gx17rhtrnjjx0bfkr03";
 $client_secret_twitch = "cdpi68gukavd5evpooq9pn3bk31ugg";
 
